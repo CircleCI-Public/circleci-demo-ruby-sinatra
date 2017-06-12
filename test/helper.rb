8789421e89af6f0615fa489203b88cd2899eceda
Zachary Scott <mail@zzak.io>
2017-06-12 16:07:08 +0900

    Hook up test reporter

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,11 @@
 require_relative '../config/app'
 require 'minitest/autorun'
+require 'minitest/ci'
 require 'rack/test'
+
+if ENV["CIRCLECI"]
+  Minitest::Ci.report_dir = "#{ENV["CIRCLE_TEST_REPORTS"]}/reports"
+end
 
 class CircleCI::TestHelper < Minitest::Test
   include Rack::Test::Methods
